17d11685b5664dd5239aedeb7634ab910e3863a8
kgaythorpe <k.gaythorpe@imperial.ac.uk>
2023-07-07 11:23:57 +0100

    tidy namespace

--- code/src/incoming_vaxtweets/script.R
+++ code/src/incoming_vaxtweets/script.R
@@ -1,7 +1,7 @@
 df <- read.csv("vax_tweets.csv", stringsAsFactors = FALSE)
 
-df <- df %>% mutate(user_in_uk = grepl("United Kingdom|GB|UK|England|London", user_location))
+df <- df %>% dplyr::mutate(user_in_uk = grepl("United Kingdom|GB|UK|England|London", user_location))
 
-df <- df %>% mutate(date = lubridate::dmy_hm(date))
+df <- df %>% dplyr::mutate(date = lubridate::dmy_hm(date))
 
 saveRDS(df, "dat.rds")
